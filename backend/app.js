5835373f3f4594e955a60170f643eb7d76f67d5f
Raihan <raihanregita@gmail.com>
2025-12-18 10:26:50 +0700

    bug stat 2e

--- backend/app.js
+++ backend/app.js
@@ -221,12 +221,6 @@ app.get("/api/videos", (req, res) => {
     });
 });
 
-app.use(express.static(path.join(__dirname, "../dist")));
-
-app.get(/.*/, (req, res) => {
-    res.sendFile(path.join(__dirname, "../dist", "index.html"));
-});
-
 // const PORT = process.env.PORT || 5000;
 // app.listen(PORT, "0.0.0.0", () => {
 //     console.log(`✅ Server running on port ${PORT}`);
